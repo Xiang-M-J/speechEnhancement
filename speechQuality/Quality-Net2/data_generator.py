2b28c3e92ba4e5adcd5a7ba50b16bc38022075f6
Xiang-M-J <3291493948@qq.com>
2024-05-06 13:38:28 +0800

    fix

--- speechQuality/Quality-Net2/data_generator.py
+++ speechQuality/Quality-Net2/data_generator.py
@@ -1,4 +1,4 @@
-###生成不同噪声下不同信噪比的语音
+# 生成不同噪声下不同信噪比的语音
 
 
 import os
@@ -7,7 +7,9 @@ import soundfile as sf
 import random
 import multiprocessing
 
-os.environ["CUDA_VISIBLE_DEVICES"]='0'
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+
+
 def ge_NoiseData(clean_data, noise_data, snr):
     """
     利用纯净语音和噪声产生混合语音
@@ -16,7 +18,7 @@ def ge_NoiseData(clean_data, noise_data, snr):
     snr:信噪比 dB
     """
     # 噪声的归一化处理
-    noise_data=noise_data/max(abs(noise_data))
+    noise_data = noise_data / max(abs(noise_data))
     # 语音合成要保证两段信号的长度相等
     if len(noise_data) < len(clean_data):
         tmp = (len(clean_data) // len(noise_data)) + 1
@@ -25,25 +27,28 @@ def ge_NoiseData(clean_data, noise_data, snr):
     else:
         noise_data = noise_data[:len(clean_data)]
 
-    clean_pwr = sum(abs(clean_data)**2)
-    noise_pwr = sum(abs(noise_data)**2)  # 计算音频信号能量
-    if noise_pwr==0:
-        noise_pwr=1
+    clean_pwr = sum(abs(clean_data) ** 2)
+    noise_pwr = sum(abs(noise_data) ** 2)  # 计算音频信号能量
+    if noise_pwr == 0:
+        noise_pwr = 1
 
-    coefficient = np.sqrt((clean_pwr/(10**(snr / 10)))/noise_pwr)  # 噪声系数计算
+    coefficient = np.sqrt((clean_pwr / (10 ** (snr / 10))) / noise_pwr)  # 噪声系数计算
     new_data = clean_data + coefficient * noise_data
-    if max(abs(new_data))>32768:
-        new_data = (new_data/max(abs(new_data)))*32768
+    if max(abs(new_data)) > 32768:
+        new_data = (new_data / max(abs(new_data))) * 32768
     return new_data
+
 
 # 生成音频采样率和幅度数组
 def generate_audio_data(path):
-    sig,sample_rate=sf.read(path)
-    sample_sig_list=[sample_rate,sig]
+    sig, sample_rate = sf.read(path)
+    sample_sig_list = [sample_rate, sig]
     return sample_sig_list
 
-def write_audio_data(new_path,data_list):
-    sf.write(file=new_path,data=data_list,samplerate=16000)
+
+def write_audio_data(new_path, data_list):
+    sf.write(file=new_path, data=data_list, samplerate=16000)
+
 
 def ListRead(filelist):
     f = open(filelist, 'r')
@@ -52,46 +57,43 @@ def ListRead(filelist):
         Path = Path + [line[0:-1]]
     return Path
 
+
 def start_fun(clean_path):
     for i in range(len(clean_path)):
         file_name = os.path.basename(clean_path[i])
         for j in range(len(noisy_audio)):
-            snr_len=len(snr_list)
+            snr_len = len(snr_list)
             for k in range(snr_len):
-                new_data = ge_NoiseData(generate_audio_data(clean_path[i])[1], generate_audio_data(noisy_audio[j])[1],snr_list[k])
+                new_data = ge_NoiseData(generate_audio_data(clean_path[i])[1], generate_audio_data(noisy_audio[j])[1],
+                                        snr_list[k])
                 print(str(new_path_list[j * snr_len + k]) + str(file_name))
 
                 # 文件路径+文件名
                 write_audio_data(str(new_path_list[j * snr_len + k]) + str(file_name), new_data)
 
 
-
-clean_audio_all=ListRead('/home/superwu/PycharmProjects/TIMIT/train_audio.list')
-noisy_audio_all=ListRead('/home/superwu/PycharmProjects/TIMIT/noisy_recording.list')
-new_path_list=ListRead("/home/superwu/PycharmProjects/TIMIT/directory_noisy.list") #更改新路径
+clean_audio_all = ListRead('/home/superwu/PycharmProjects/TIMIT/train_audio.list')
+noisy_audio_all = ListRead('/home/superwu/PycharmProjects/TIMIT/noisy_recording.list')
+new_path_list = ListRead("/home/superwu/PycharmProjects/TIMIT/directory_noisy.list")  #更改新路径
 # random.shuffle(clean_audio_all)
 random.shuffle(noisy_audio_all)
 # 0:250为纯净语音，250：450生成enhance组，450：650生成noisy组，
 # 测试集由test-audio的前一百个语音生成，四种噪声，六种信噪比snr_list=[-6,0,6,12,18,24]
-clean_audio=clean_audio_all[450:650]
-noisy_audio=noisy_audio_all # 目前使用70个语音
-snr_list=[0,10,18,21,24,27,30] # 手动设置需要的SNR
+clean_audio = clean_audio_all[450:650]
+noisy_audio = noisy_audio_all  # 目前使用70个语音
+snr_list = [0, 10, 18, 21, 24, 27, 30]  # 手动设置需要的SNR
 
-
-if __name__=="__main__":
-    cpu_num=multiprocessing.cpu_count()
-    chunks=len(clean_audio)//cpu_num+1
-    process=[]
+if __name__ == "__main__":
+    cpu_num = multiprocessing.cpu_count()
+    chunks = len(clean_audio) // cpu_num + 1
+    process = []
     for i in range(cpu_num):
-        temp=clean_audio[i*chunks:(i+1)*chunks]
-        p=multiprocessing.Process(target=start_fun,args=(temp,))
+        temp = clean_audio[i * chunks:(i + 1) * chunks]
+        p = multiprocessing.Process(target=start_fun, args=(temp,))
         p.start()
         process.append(p)
     for p in process:
         p.join()
-
-
-
 
     # for i in range(len(clean_audio)):
     #     file_name = os.path.basename(clean_audio[i])
@@ -101,6 +103,3 @@ if __name__=="__main__":
     #
     #             # 文件路径+文件名
     #             write_audio_data(str(new_path_list[j*8+k])+str(file_name),new_data)
-
-
-
---
+++ speechQuality/Quality-Net2/load_data.py
@@ -0,0 +1,87 @@
+import os
+import random
+from glob import glob
+
+import numpy as np
+import soundfile
+import multiprocessing
+
+from pyarrow import fs
+
+clean_wav_path = r"D:\work\speechEnhancement\datasets\TIMIT"
+noise_wav_path = r"D:\work\speechEnhancement\datasets\Demand"
+
+
+class DemandNoise:
+    def __init__(self, path, fs):
+        self.path = path
+        self.fs = fs
+        wav_dirs = os.listdir(self.path)
+        self.noise_types = wav_dirs
+        self.wav = {}
+        for wav_dir in wav_dirs:
+            if wav_dir.endswith(f"{fs}k"):
+                self.wav[wav_dir] = os.listdir(os.path.join(path, wav_dir, wav_dir[:-4]))
+
+    def add_noise(self, clean_path, snr=10):
+        noise_type = random.choice(self.noise_types)
+        noise_path = random.choice(self.wav[noise_type])
+        clean_data, sr = soundfile.read(clean_path)
+        noise_data, sr = soundfile.read(os.path.join(self.path, noise_type, noise_type[:-4], noise_path))
+        noise_data = noise_data / np.max(np.abs(noise_data))
+
+        if len(noise_data) < len(clean_data):
+            tmp = (len(clean_data) // len(noise_data)) + 1
+            noise_data = np.tile(noise_data, tmp)
+            noise_data = noise_data[:len(clean_data)]
+        else:
+            noise_data = noise_data[:len(clean_data)]
+        clean_pwr = sum(abs(clean_data) ** 2)
+        noise_pwr = sum(abs(noise_data) ** 2)  # 计算音频信号能量
+        if noise_pwr == 0:
+            noise_pwr = 1
+
+        coefficient = np.sqrt((clean_pwr / (10 ** (snr * 0.1))) / noise_pwr)  # 噪声系数计算
+        new_data = clean_data + coefficient * noise_data
+        if max(abs(new_data)) > 32768:
+            new_data = (new_data / max(abs(new_data))) * 32768
+        return new_data
+
+
+def get_new_path(prefix: str, old_path: str, snr: int):
+    new_path = "_".join(old_path.split("\\")[-3:])
+    return os.path.join(prefix, new_path[:-8] + "_" + str(snr) + ".wav")
+
+
+def start_fun(clean_path):
+    for i in range(len(clean_path)):
+        for k in range(snr_len):
+            new_data = demandNoise.add_noise(clean_path[i], snr_list[k])
+            new_path = get_new_path(clean_path[i], snr_list[k])
+            soundfile.write(new_path, new_data, samplerate=fs)
+
+
+if __name__ == "__main__":
+    fs = 16
+    demandNoise = DemandNoise(noise_wav_path, fs)
+
+    train_wav = glob("{}\\data\\TRAIN/*/*/*.wav".format(clean_wav_path), recursive=True)
+    train_wav = np.array(train_wav[1::2])
+    random_index = np.random.permutation(len(train_wav))
+    clean_wav = train_wav[random_index[:250]]
+    noise_wav = train_wav[random_index[250:500]]
+    enhance_wav = train_wav[random_index[500:750]]
+
+    print(get_new_path(r"D:\work\speechEnhancement\speechQuality\Quality-Net2\noise_wavs",clean_wav[0], 10))
+
+    snr_list = [5 * i for i in range(-2, 6)]
+    snr_len = len(snr_list)
+    with open("list/clean.list", "w") as f:
+        for wav in clean_wav:
+            f.write(wav + "\n")
+    with open("list/noise.list", "w") as f:
+        for wav in noise_wav:
+            f.write(wav + "\n")
+    with open("list/enhance.list", "w") as f:
+        for wav in enhance_wav:
+            f.write(wav + "\n")
